f23c644d05c5f0c587dd51d314d69d38b15e7729
mkleine <mkleine>
2004-07-04 11:28:33 +0000

    Initial commit after renaming prototype -> workbench.

---
+++ gnogito/javaprojekte/gnogitoworkbench/src/gnogitoworkbench/GnogitoPerspective.java
@@ -0,0 +1,18 @@
+/*
+ * Created on 12.02.2004
+ *
+ */
+package gnogitoworkbench;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+/**
+ * @author matthias
+ * 
+ * created first in project gnogitoworkbench
+ */
+public class GnogitoPerspective implements IPerspectiveFactory {
+	public GnogitoPerspective() {
+	}
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
---
+++ gnogito/javaprojekte/gnogitoworkbench/src/gnogitoworkbench/GnogitoWorkbench.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 12.02.2004
+ *
+ */
+package gnogitoworkbench;
+import org.eclipse.core.boot.IPlatformRunnable;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+/**
+ * @author matthias
+ * 
+ * created first in project gnogitoworkbench
+ */
+public class GnogitoWorkbench implements IPlatformRunnable {
+	public Object run(Object args) {
+		WorkbenchAdvisor workbenchAdvisor = new GnogitoWorkbenchAdvisor();
+		Display display = PlatformUI.createDisplay();
+		int returnCode = PlatformUI.createAndRunWorkbench(display,
+				workbenchAdvisor);
+		if (returnCode == PlatformUI.RETURN_RESTART) {
+			return IPlatformRunnable.EXIT_RESTART;
+		} else {
+			return IPlatformRunnable.EXIT_OK;
+		}
+	}
+}
